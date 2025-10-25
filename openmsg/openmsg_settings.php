7ff37cff7e8c629ab697c17ea3cd50d21ff172f1
Openmsg-io <connect@openmsg.io>
2025-10-25 23:42:53 +0100

    Update openmsg_settings.php

--- openmsg/openmsg_settings.php
+++ openmsg/openmsg_settings.php
@@ -3,14 +3,12 @@
 
 // enter your domain here. This needs to be the domain used in your account addresses and where the /openmsg/ protocol folder is hosted
 $my_openmsg_domain = "enter_your_domain_in_settings_file.com"; 
-$sandbox = FALSE;
-if($sandbox == TRUE) $sandbox_dir = "/sandbox";
 
-// Connect to your database
+// Connect to your database // Example only ... Please dont really store your DB details here...!
 /*
 $DBhost = "localhost";
 $DBuser = "username";
-$DBpass = "password";
+$DBpass = "password"; // Please dont really store your DB password here...!
 $DBName = "myDB";
 
 $db = new mysqli($DBhost,$DBuser,$DBpass,$DBName);
@@ -28,3 +26,4 @@ require ($_SERVER["DOCUMENT_ROOT"]."/includes/test_database_connection.php"); //
 
 
 ?>
+
