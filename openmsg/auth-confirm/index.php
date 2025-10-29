cf5c8780213f3026336d078cce1592475b619618
Openmsg-io <connect@openmsg.io>
2025-10-29 21:21:32 +0000

    Update index.php

--- openmsg/auth-confirm/index.php
+++ openmsg/auth-confirm/index.php
@@ -7,13 +7,13 @@ require "../openmsg_settings.php";
 $data = json_decode(file_get_contents("php://input"), true);
 
 $other_openmsg_address = $data["receiving_openmsg_address"]; 
-$pass_code = $data["pass_code"]; 
+$passcode_hash = $data["passcode_hash"]; 
 
 
-function auth_confirm ($db, $other_openmsg_address, $pass_code){
-    // Query database to check for a pending authorization request with openmsg_address and pass_code 
-    $stmt = $db->prepare("SELECT UNIX_TIMESTAMP(timestamp) FROM openmsg_handshakes WHERE other_openmsg_address = ? AND pass_code = ?");
-    $stmt->bind_param("ss", $other_openmsg_address, $pass_code);
+function auth_confirm ($db, $other_openmsg_address, $passcode_hash){
+    // Query database to check for a pending authorization request with openmsg_address and passcode_hash 
+    $stmt = $db->prepare("SELECT UNIX_TIMESTAMP(timestamp) FROM openmsg_handshakes WHERE other_openmsg_address = ? AND passcode_hash = ?");
+    $stmt->bind_param("ss", $other_openmsg_address, $passcode_hash);
     $stmt->execute();  // To Do: Un-comment
     $stmt->store_result();
     $stmt->bind_result($initation_timestamp);
@@ -22,26 +22,27 @@ function auth_confirm ($db, $other_openmsg_address, $pass_code){
     $stmt->close();
     
     if($matching_handshakes == 0) { 
-        // If the pass_code isnt present for the openmsg_address then return error and error_message
-        $response = array("error"=>TRUE, "error_message"=>"unknown pending authorization with $other_openmsg_address, $pass_code"); 
+        // If the passcode_hash isnt present for the openmsg_address then return error and error_message
+        $response = array("error"=>TRUE, "error_message"=>"unknown pending authorization with $other_openmsg_address, $passcode_hash"); 
         return($response);
     }
     if($initation_timestamp < time()-60){
-        // If the pass_code has expired then return error and error_message
+        // If the handshake has expired then return error and error_message
         $response = array("error"=>TRUE, "error_message"=>"expired handshake, over 60 seconds old"); 
         return($response);
     }
     
     // The pending handshake has been verified once. It should now be deleted at this stage so it cant be re-used
-    $stmt = $db->prepare("DELETE FROM openmsg_handshakes WHERE other_openmsg_address = ? AND pass_code = ? LIMIT 1");
-    $stmt->bind_param("ss", $other_openmsg_address, $pass_code);
+    $stmt = $db->prepare("DELETE FROM openmsg_handshakes WHERE other_openmsg_address = ? AND passcode_hash = ? LIMIT 1");
+    $stmt->bind_param("ss", $other_openmsg_address, $passcode_hash);
     $stmt->execute(); // To Do: Un-comment
      
     $response = array("success"=>TRUE); 
     return($response);
 }
 
-$response = auth_confirm ($db, $other_openmsg_address, $pass_code); // $db = database connection
+$response = auth_confirm ($db, $other_openmsg_address, $passcode_hash); // $db = database connection
 echo json_encode($response);  
 
 ?>
+
